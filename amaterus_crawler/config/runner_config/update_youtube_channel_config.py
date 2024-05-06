015b9788c2ba81b4cf17bd1100c55aff103f2b67
aoirint <aoirint@gmail.com>
2024-05-06 23:36:55 +0900

    UpdateYoutubeChannelConfig: add hasura_admin_secret, hasura_role

--- amaterus_crawler/config/runner_config/update_youtube_channel_config.py
+++ amaterus_crawler/config/runner_config/update_youtube_channel_config.py
@@ -1,12 +1,15 @@
-from typing import Annotated, Literal
+from typing import Literal
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 
 class UpdateYoutubeChannelConfigOptions(BaseModel):
     override_hasura: bool = False
     hasura_url: str | None = None
     hasura_access_token: str | None = None
+    hasura_admin_secret: str | None = None
+    hasura_role: str | None = None
+
     override_youtube_api_key: bool = False
     youtube_api_key: str | None = None
 
@@ -14,4 +17,4 @@ class UpdateYoutubeChannelConfigOptions(BaseModel):
 class UpdateYoutubeChannelConfig(BaseModel):
     type: Literal["update_youtube_channel"]
     enabled: bool
-    options: Annotated[UpdateYoutubeChannelConfigOptions | None, Field(default=None)]
+    options: UpdateYoutubeChannelConfigOptions | None = None
